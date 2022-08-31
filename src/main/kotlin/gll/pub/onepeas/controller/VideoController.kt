7828d73e665c28b90240f2b8cd2723eacd50291f
贡磊磊 <>
2022-08-31 14:30:25 +0800

    --

--- src/main/kotlin/gll/pub/onepeas/controller/VideoController.kt
+++ src/main/kotlin/gll/pub/onepeas/controller/VideoController.kt
@@ -8,6 +8,7 @@ import gll.pub.onepeas.util.JsonUtils
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
+import org.springframework.data.jpa.domain.Specification
 
 import java.util.Optional
 
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.PathVariable
+import javax.persistence.criteria.*
 
 @RestController
 @RequestMapping("/api/video", produces = ["application/json"])
@@ -49,6 +51,82 @@ class OnePeasController (private val userRepository: VideoRepository) {
 //        val sort = Sort.by(arrayOf(order1).toMutableList())
 //        val pageable: PageRequest = PageRequest.of(pageNo, 10, sort);
         val pageable: PageRequest = PageRequest.of(page-1, 10);
+        val pages = userRepository.findAll(object :Specification<VideoEntity>{
+            override fun toPredicate(root: Root<VideoEntity>, p1: CriteriaQuery<*>, criteriaBuilder: CriteriaBuilder): Predicate? {
+                val userName: Path<String> = root["url"]
+                return criteriaBuilder.like(userName, "%${key}%")
+            }
+        },pageable)
+        println("查询总页数："+pages.totalPages)
+        println("查询总记录数："+pages.totalElements)
+        println("查询当前第几页："+pages.number +1);// 注意 0开始
+        println("查询当前页面的集合："+pages.content)
+        println("查询当前页面的记录数："+pages.numberOfElements);
+        return pages
+    }
+
+    fun save(videoEntityList:List<VideoJsonBean>,path: String){
+        videoEntityList.forEach {
+            val videoEntity = VideoEntity()
+            videoEntity.title = it.name
+            if (path==""){
+                videoEntity.url = "movies/${it.url}"
+            }else{
+                videoEntity.url = "movies/$path/${it.url}"
+            }
+            videoEntity.size = it.size
+            userRepository.save(videoEntity)
+        }
+    }
+
+    @GetMapping("/addAll")
+    suspend fun addAll(): Page<VideoEntity> {
+//        val bean = FileUtil.getFileNameList("影视总")
+//        save(bean,"")
+//
+//        val bean1 = FileUtil.getFileNameList("灵魂摆渡-第一季")
+//        save(bean1,"灵H摆D 1-3/第一季")
+//
+//        val bean2 = FileUtil.getFileNameList("灵魂摆渡-第二季")
+//        save(bean2,"灵H摆D 1-3/第二季")
+//
+//        val bean3 = FileUtil.getFileNameList("灵魂摆渡-第三季")
+//        save(bean3,"灵H摆D 1-3/第三季")
+//
+//        val bean4 = FileUtil.getFileNameList("灵魂摆渡-黄泉")
+//        save(bean4,"灵H摆D 1-3/黄泉")
+//
+//        val bean5 = FileUtil.getFileNameList("秦时明月-百步飞剑")
+//        save(bean5,"秦时明月(2007) [主系列六部全+天行九歌]/百步飞剑")
+//
+//        val bean6 = FileUtil.getFileNameList("秦时明月-沧海横流")
+//        save(bean6,"秦时明月(2007) [主系列六部全+天行九歌]/沧海横流")
+//
+//        val bean7 = FileUtil.getFileNameList("秦时明月-君临天下")
+//        save(bean7,"秦时明月(2007) [主系列六部全+天行九歌]/君临天下")
+//
+//        val bean8 = FileUtil.getFileNameList("秦时明月-天行九歌")
+//        save(bean8,"秦时明月(2007) [主系列六部全+天行九歌]/天行九歌")
+//
+//        val bean9 = FileUtil.getFileNameList("秦时明月-万里长城")
+//        save(bean9,"秦时明月(2007) [主系列六部全+天行九歌]/万里长城")
+//
+//        val bean10 = FileUtil.getFileNameList("秦时明月-夜尽天明")
+//        save(bean10,"秦时明月(2007) [主系列六部全+天行九歌]/夜尽天明")
+//
+//        val bean11 = FileUtil.getFileNameList("秦时明月-诸子百家")
+//        save(bean11,"秦时明月(2007) [主系列六部全+天行九歌]/诸子百家")
+//
+//        val bean12 = FileUtil.getFileNameList("职场")
+//        save(bean12,"职场生存指南：深度拆解困住1000万人的52个职场难题（完结）")
+//
+//        val bean13 = FileUtil.getFileNameList("鬼喊抓鬼")
+//        save(bean13,"G.鬼喊抓鬼 牛大宝（全227集）")
+
+
+        val order1 = Sort.Order(Sort.Direction.DESC, "id")
+        val sort = Sort.by(arrayOf(order1).toMutableList())
+        val pageable: PageRequest = PageRequest.of(0, 1000, sort);
         val pages = userRepository.findAll(pageable);
         println("查询总页数："+pages.totalPages)
         println("查询总记录数："+pages.totalElements)
@@ -58,17 +136,19 @@ class OnePeasController (private val userRepository: VideoRepository) {
         return pages
     }
     @GetMapping("/add")
-    suspend fun add(): Page<VideoEntity> {
-        val json = FileUtil.readJSON("src/main/resources/data1.json").replace("last modified","lastModified")
-        val bean = JsonUtils.fromJsonList(json,VideoJsonBean::class.java)
-        bean?.forEach {
-            val videoEntity = VideoEntity()
-            videoEntity.title = it.name
-            videoEntity.url = "http://49.232.198.163:8080/movies/${it.name}"
-            videoEntity.size = it.size
-
-            userRepository.save(videoEntity)
-        }
+    suspend fun add(json: String, path: String): Page<VideoEntity> {
+//        val bean = FileUtil.getFileNameList(json)
+//        bean.forEach {
+//            val videoEntity = VideoEntity()
+//            videoEntity.title = it.name
+//            if (path==""){
+//                videoEntity.url = "movies/${it.url}"
+//            }else{
+//                videoEntity.url = "movies/$path/${it.url}"
+//            }
+//            videoEntity.size = it.size
+//            userRepository.save(videoEntity)
+//        }
 
 
 
@@ -86,4 +166,4 @@ class OnePeasController (private val userRepository: VideoRepository) {
     }
 
 }
-data class VideoJsonBean(val name:String,val lastModified:String,val size:String)
+data class VideoJsonBean(val name:String,val url:String,val lastModified:String,val size:String)
--- src/main/kotlin/gll/pub/onepeas/util/FileUtil.kt
+++ src/main/kotlin/gll/pub/onepeas/util/FileUtil.kt
@@ -1,5 +1,7 @@
 package gll.pub.onepeas.util
 
+import com.google.gson.Gson
+import gll.pub.onepeas.controller.VideoJsonBean
 import java.io.File
 
 object FileUtil {
@@ -7,10 +9,47 @@ object FileUtil {
         return File(fileName).readText()
 
     }
+
+    fun getFileNameList(json:String):List<VideoJsonBean>{
+        val strList = arrayListOf<String>(".mkv",".mp4",".flv",".rmvb")
+        val json = FileUtil.readJSON("src/main/resources/$json.json").replace("last modified","lastModified")
+        val list = JsonUtils.fromJsonList<JSON>(json,JSON::class.java)
+        val resultList = arrayListOf<VideoJsonBean>()
+        list?.forEach {
+            val name = it.name.run {
+                var replaceStr:String?=null
+                strList.forEach {
+                    if (this.contains(it,true)){
+                        replaceStr = this.replace(it,"",true)
+                    }
+                }
+                replaceStr
+            }
+            if (name!=null){
+                resultList.add(VideoJsonBean(name,it.name,it.lastModified,it.size))
+            }
+        }
+        return resultList
+    }
 }
 
 fun main() {
+    val strList = arrayListOf<String>(".mkv",".mp4",".flv",".rmvb")
     val json = FileUtil.readJSON("src/main/resources/data.json").replace("last modified","lastModified")
-    println(json)
+    val list = JsonUtils.fromJsonList<JSON>(json,JSON::class.java)
+    list?.forEach {
+        val name = it.name.run {
+            var replaceStr:String?=null
+            strList.forEach {
+                if (this.contains(it,true)){
+                    replaceStr = this.replace(it,"",true)
+                }
+            }
+            replaceStr
+        }
+        if (name!=null){
+            println(name)
+        }
+    }
 }
 data class JSON(val name:String,val lastModified:String,val size:String)
