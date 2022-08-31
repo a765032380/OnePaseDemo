c6f7d454535e22617f47e599f66d6b2e98e9cc0c
贡磊磊 <>
2022-08-31 15:42:08 +0800

    --

--- src/main/kotlin/gll/pub/onepeas/util/FileUtil.kt
+++ src/main/kotlin/gll/pub/onepeas/util/FileUtil.kt
@@ -1,6 +1,5 @@
 package gll.pub.onepeas.util
 
-import com.google.gson.Gson
 import gll.pub.onepeas.controller.VideoJsonBean
 import java.io.File
 
@@ -30,26 +29,6 @@ object FileUtil {
             }
         }
         return resultList
-    }
-}
-
-fun main() {
-    val strList = arrayListOf<String>(".mkv",".mp4",".flv",".rmvb")
-    val json = FileUtil.readJSON("src/main/resources/data.json").replace("last modified","lastModified")
-    val list = JsonUtils.fromJsonList<JSON>(json,JSON::class.java)
-    list?.forEach {
-        val name = it.name.run {
-            var replaceStr:String?=null
-            strList.forEach {
-                if (this.contains(it,true)){
-                    replaceStr = this.replace(it,"",true)
-                }
-            }
-            replaceStr
-        }
-        if (name!=null){
-            println(name)
-        }
     }
 }
 data class JSON(val name:String,val lastModified:String,val size:String)
